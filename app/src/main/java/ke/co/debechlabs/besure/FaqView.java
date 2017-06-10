e0e518c0b81c9acf9fe9a4063f8bb32b5eff7d0c
WillyMareka <marekawilly@gmail.com>
2017-06-10 16:25:54 +0300

    Images now loading in the FAQ page, now for the styling

--- app/src/main/java/ke/co/debechlabs/besure/FaqView.java
+++ app/src/main/java/ke/co/debechlabs/besure/FaqView.java
@@ -1,16 +1,28 @@
 package ke.co.debechlabs.besure;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class FaqView extends AppCompatActivity {
-    TextView faqImage, faqID, faqQuestion, faqAnswer, faqStatus;
+    TextView txtfaqImage, faqID, faqQuestion, faqAnswer, faqStatus;
+    ImageView faqImage;
+    String image = "";
 
     private Toolbar toolbar;
 
@@ -19,7 +31,7 @@ public class FaqView extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_faq_view);
 
-        toolbar= (Toolbar) findViewById(R.id.toolbar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         toolbar.setTitle("FAQs");
@@ -28,7 +40,8 @@ public class FaqView extends AppCompatActivity {
         actionBar.setHomeButtonEnabled(true);
         actionBar.setDisplayHomeAsUpEnabled(true);
 
-        faqImage = (TextView) findViewById(R.id.faqImage);
+//        faqImage = (TextView) findViewById(R.id.faqImage);
+        faqImage = (ImageView) findViewById(R.id.faqImage);
         faqQuestion = (TextView) findViewById(R.id.faqQuestion);
         faqAnswer = (TextView) findViewById(R.id.faqAnswer);
 
@@ -36,15 +49,59 @@ public class FaqView extends AppCompatActivity {
         String faqid = extras.getString("faqid");
         String question = extras.getString("question");
         String answer = extras.getString("answer");
-        String image = extras.getString("image");
+        image = extras.getString("image");
         String status = String.valueOf(extras.getInt("status"));
 
 //        Toast.makeText(getApplicationContext(),question,Toast.LENGTH_LONG).show();
 
-        faqImage.setText(image);
+//        faqImage.setText(image);
         faqQuestion.setText(question);
         faqAnswer.setText(answer);
 
+//        getBitmapFromURL(image);
+        new ImageLoadTask().execute();
+//        faqImage.setImageBitmap(getBitmapFromURL(image));
 
     }
+
+
+    public class ImageLoadTask extends AsyncTask<Void, Void, Bitmap> {
+
+        private String url;
+        private ImageView imageView;
+
+        public ImageLoadTask() {
+            this.url = url;
+            this.imageView = imageView;
+        }
+
+        @Override
+        protected Bitmap doInBackground(Void... params) {
+            try {
+                URL urlConnection = new URL(image);
+                HttpURLConnection connection = (HttpURLConnection) urlConnection
+                        .openConnection();
+                connection.setDoInput(true);
+                connection.connect();
+                InputStream input = connection.getInputStream();
+                Bitmap myBitmap = BitmapFactory.decodeStream(input);
+                return myBitmap;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap result) {
+            super.onPostExecute(result);
+            faqImage.setImageBitmap(result);
+        }
+
+    }
+
+
 }
+
+
+
